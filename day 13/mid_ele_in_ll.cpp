6ff9899b8854feace61361e3228b852cd5005c90
vamsi9786 <vamsi6319@gmail.com>
2026-03-03 00:45:50 +0530

    Add function to find middle node in linked list
    Implemented a function to return the middle node value of a linked list, handling both odd and even lengths.

---
+++ day 13/mid_ele_in_ll.cpp
@@ -0,0 +1,36 @@
+You are given the head of a linked list, You have to return the value of the middle node of the linked list.
+
+If the number of nodes is odd, return the middle node value.
+If the number of nodes is even, there are two middle nodes, so return the second middle node value.
+
+/*
+class Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}; */
+
+class Solution {
+  public:
+    int getMiddle(Node* head) {
+        int n=0;
+        Node* t=head;
+        while(t!=NULL){
+            n++;
+            t=t->next;
+        }
+        int req=(n%2==0)?(n+2)/2:(n+1)/2;
+        int res;
+        t=head;
+        while(req--){
+            res=t->data;
+            t=t->next;
+        }
+        return res;
+    }
+};

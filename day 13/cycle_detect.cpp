653508e5282aa03ef891259ff7ce8d774082b8e6
vamsi9786 <vamsi6319@gmail.com>
2026-03-03 01:06:18 +0530

    Create cycle_detect.cpp

---
+++ day 13/cycle_detect.cpp
@@ -0,0 +1,49 @@
+Floyd's Cycle Detection
+using fast and slow pointer
+
+bool detectLoop(Node* head) {
+    if (head == NULL) return false;
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
+
+alternate
+using hash map i.e unorrdered set
+/*
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+} */
+
+class Solution {
+  public:
+    bool detectLoop(Node* head) {
+        // code here
+        if(head==NULL || head->next==NULL) return false;
+        unordered_set<Node*> vis;
+        Node* t=head;
+        while(t!=NULL){
+            if(vis.find(t)!=vis.end()) return true;
+            vis.insert(t);
+            t=t->next;
+        }
+        return false;
+    }
+};

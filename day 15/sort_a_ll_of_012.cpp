678087ff69bb8c6c2ebbd6ec11ceb3cc36970fc0
vamsi9786 <vamsi6319@gmail.com>
2026-03-06 12:34:32 +0530

    Update link in sort_a_ll_of_012.cpp

--- day 15/sort_a_ll_of_012.cpp
+++ day 15/sort_a_ll_of_012.cpp
@@ -1,4 +1,4 @@
-https://www.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it
+https://www.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1
 
 Given the head of a linked list where nodes can contain values 0s, 1s, and 2s only. 
 Your task is to rearrange the list so that all 0s appear at the beginning, followed by all 1s, and all 2s are placed at the end.

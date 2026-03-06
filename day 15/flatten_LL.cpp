dda88ae3e51ee21bd6631bf7b29338acaf8b89b2
vamsi9786 <vamsi6319@gmail.com>
2026-03-06 12:34:07 +0530

    Update link for flattening a linked list problem

--- day 15/flatten_LL.cpp
+++ day 15/flatten_LL.cpp
@@ -1,4 +1,4 @@
-https://www.geeksforgeeks.org/problems/flattening-a-linked-list
+https://www.geeksforgeeks.org/problems/flattening-a-linked-list/1
 
 Given a linked list containing n head nodes where every node in the linked list contains two pointers:
     (i) next points to the next node in the list.

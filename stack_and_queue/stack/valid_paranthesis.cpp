a5469fb32a6d115621d356ed755a3131892586a8
vamsi9786 <vamsi6319@gmail.com>
2026-03-13 23:56:24 +0530

    Update valid_paranthesis.cpp

--- stack_and_queue/stack/valid_paranthesis.cpp
+++ stack_and_queue/stack/valid_paranthesis.cpp
@@ -30,3 +30,34 @@ public:
         return false;
     }
 };
+
+
+Cleaner version
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> stk;
+
+        for(char c : s){
+
+            if(c=='(' || c=='{' || c=='[')
+                stk.push(c);
+            else{
+
+                if(stk.empty()) return false;
+
+                char top = stk.top();
+                stk.pop();
+
+                if((top=='(' && c!=')') ||
+                   (top=='{' && c!='}') ||
+                   (top=='[' && c!=']'))
+                    return false;
+            }
+        }
+
+        return stk.empty();
+    }
+};

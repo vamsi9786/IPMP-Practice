c44522e671191645624108804e25180f29741e48
vamsi9786 <vamsi6319@gmail.com>
2026-03-17 18:34:48 +0530

    Create min_add_valid_paran.cpp

---
+++ stack_and_queue/stack/min_add_valid_paran.cpp
@@ -0,0 +1,33 @@
+https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/
+
+A parentheses string is valid if and only if:
+
+It is the empty string,
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+
+For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+Return the minimum number of moves required to make s valid.
+
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n=s.length();
+        int ans=0;
+        stack<char> stk;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') stk.push(s[i]);
+            else{
+                if(!stk.empty() && stk.top()=='(') stk.pop();
+                else stk.push(s[i]);
+            }
+        }
+        while(!stk.empty()){
+            ans++;
+            stk.pop();
+        }
+        return ans;
+    }
+};

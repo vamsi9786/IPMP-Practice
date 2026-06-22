358cace14b9c3518a49b7e40cc1a797527b88eb7
vamsi9786 <vamsi6319@gmail.com>
2026-06-22 14:23:11 +0530

    Update link_list_rev_k_nos.cpp

--- Linked List/day 13/link_list_rev_k_nos.cpp
+++ Linked List/day 13/link_list_rev_k_nos.cpp
@@ -15,6 +15,36 @@ class Node {
     }
 };
 */
+
+class Solution {
+  public:
+    Node *reverseKGroup(Node *head, int k) {
+        // code here
+        if(head==NULL) return NULL;
+        Node* prev=NULL;
+        Node* curr=head;
+        Node* next=NULL;
+        int cnt=0;
+        while(curr!=NULL && cnt<k){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            cnt++;
+        }
+        if(next!=NULL){
+            head->next=reverseKGroup(next,k);
+        }
+        return prev;
+    }
+};
+  
+
+
+
+
+
+  
 class Solution {
   public:
     Node *reverseKGroup(Node *head, int k) {

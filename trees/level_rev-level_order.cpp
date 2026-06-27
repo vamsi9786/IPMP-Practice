010bc71a05846bce7ad2f3a47a325265f7da5e89
vamsi9786 <vamsi6319@gmail.com>
2026-06-27 23:44:49 +0530

    Add level_rev-level_order.cpp file

--- trees/level_rev-level_order.cpp
+++ trees/level_rev-level_order.cpp
@@ -36,3 +36,51 @@ public:
         return res;
     }
 };
+
+
+2. Reverse level order
+
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> reverseLevelOrder(Node *root) {
+        // code here
+        vector<int> res;
+        if(root==NULL) return res;
+        
+        queue<Node*> que;
+        que.push(root);
+        
+        stack<int> stk;
+        
+        while(!que.empty()){
+            Node* node=que.front();
+            que.pop();
+            
+            stk.push(node->data);  // or use res.insert(res.begin(),node->data) but it takes O(n2)
+            
+            if(node->right) que.push(node->right);
+            if(node->left) que.push(node->left);
+        }
+        
+        while(!stk.empty()){
+            res.push_back(stk.top());
+            stk.pop();
+        }
+        return res;
+    }
+};

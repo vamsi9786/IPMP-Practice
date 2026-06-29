946b5a0b2912c3e1fd1ce1837921b8686db103f6
vamsi9786 <vamsi6319@gmail.com>
2026-06-29 14:43:29 +0530

    Update BST_LCA_least_common_ancestor(LCA).cpp

--- trees/day 22/BST_LCA_least_common_ancestor(LCA).cpp
+++ trees/day 22/BST_LCA_least_common_ancestor(LCA).cpp
@@ -6,3 +6,28 @@ According to the definition of LCA on Wikipedia:
 “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants 
   (where we allow a node to be a descendant of itself).”
 
+Idea: if both exist is same subtree return LCA(subtree,p,q)
+      else return root
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val > p->val && root->val > q->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        else if(root->val < p->val && root->val < q->val){
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        return root;
+    }
+};

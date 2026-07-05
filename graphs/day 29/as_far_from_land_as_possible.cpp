40f698e721c9f2e68ee7fc2438ba59cef8a7a443
vamsi9786 <vamsi6319@gmail.com>
2026-07-05 19:05:06 +0530

    Update as_far_from_land_as_possible.cpp

--- graphs/day 29/as_far_from_land_as_possible.cpp
+++ graphs/day 29/as_far_from_land_as_possible.cpp
@@ -6,6 +6,10 @@ If no land or water exists in the grid, return -1.
 
 The distance used in this problem is the Manhattan distance: the distance between two cells (x0, y0) and (x1, y1) is |x0 - x1| + |y0 - y1|.
 
+Idea: Similar to bridge question.
+      Do Multi-source BFS (take all 1s into BFS queue).
+      One BFS level ===> incr dist by 1. (dist++)
+
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
